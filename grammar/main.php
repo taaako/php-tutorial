98856cdca67418dc7b3389f54ae3f3477d9d96df
taaako <ta12ko.sa@gmail.com>
2021-05-19 16:42:55 +0900

    dot install #12

--- grammar/main.php
+++ grammar/main.php
@@ -1,12 +1,23 @@
 <?php
 
-// $name = 'sato';
-// $name = 'dotinstall';
+// データ型
+// string / int / float / null / bool / array / object
 
-// 定数は大文字 define() と const
-// 定数に再代入しようとするとNoticeという警告になる
-define('NAME', 'sato');
-echo NAME . PHP_EOL;
+$a = 'hello';
+$b = 10;
+$c = -1.3;
+$d = null;
+$e = true;
 
-const CNAME = 'sato';
-echo CNAME . PHP_EOL;
+// var_dump($a);
+// var_dump($b);
+// var_dump($c);
+// var_dump($d);
+// var_dump($e);
+
+// 型のキャスト
+$a = (float)10;
+$b = (string)1.3;
+
+var_dump($a);
+var_dump($b);
