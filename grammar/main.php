1c3873a9402ab3cba34171fc018acd320c53c861
taaako <ta12ko.sa@gmail.com>
2021-05-19 16:55:43 +0900

    dot install #14

--- grammar/main.php
+++ grammar/main.php
@@ -1,23 +1,14 @@
 <?php
 
-// データ型
-// string / int / float / null / bool / array / object
+// 単一の値でのfalse判定
+// false / 0 / 0.0 / '0' / '' / null / []
 
-$a = 'hello';
-$b = 10;
-$c = -1.3;
-$d = null;
-$e = true;
+$score = 40;
 
-// var_dump($a);
-// var_dump($b);
-// var_dump($c);
-// var_dump($d);
-// var_dump($e);
-
-// 型のキャスト
-$a = (float)10;
-$b = (string)1.3;
-
-var_dump($a);
-var_dump($b);
+if ($score >= 80) {
+    echo 'Great!' . PHP_EOL;
+} else if ($score >= 60) {
+    echo 'Good' . PHP_EOL;
+} else {
+    echo 'OK' . PHP_EOL;
+}
