c3a1c2d5c1e10cdb5fd3df0e49bddb9aa72522a9
taaako <ta12ko.sa@gmail.com>
2021-05-19 17:05:29 +0900

    dot install #16

--- grammar/main.php
+++ grammar/main.php
@@ -1,14 +1,16 @@
 <?php
 
-// 単一の値でのfalse判定
-// false / 0 / 0.0 / '0' / '' / null / []
+// if === の時の条件分岐 -> switch
+$signal = 'red';
 
-$score = 40;
-
-if ($score >= 80) {
-    echo 'Great!' . PHP_EOL;
-} else if ($score >= 60) {
-    echo 'Good' . PHP_EOL;
-} else {
-    echo 'OK' . PHP_EOL;
+switch ($signal) {
+    case 'red':
+        echo 'Stop!' . PHP_EOL;
+        break;
+    case 'yellow':
+        echo 'Caution' . PHP_EOL;
+        break;
+    case 'blue':
+        echo 'Go' . PHP_EOL;
+        break;
 }
