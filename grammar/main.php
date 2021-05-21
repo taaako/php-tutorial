08b45010f3ce29fcf8bec823f393db08fa246709
taaako <ta12ko.sa@gmail.com>
2021-05-21 10:45:01 +0900

    dot install #25-#26

--- grammar/main.php
+++ grammar/main.php
@@ -1,36 +1,24 @@
 <?php
 
-// #22
-function showAd($message = 'Ad') // 仮引数 / '初期値'
+$sum = function ($a, $b, $c) { // 無名関数
+    return $a + $b + $c;
+};
+
+echo $sum(100, 300, 500) . PHP_EOL;
+
+// #26
+function cal($a, $b, $c)
 {
-    echo '------------' . PHP_EOL;
-    echo '-----' . $message . '-----' . PHP_EOL;
-    echo '------------' . PHP_EOL;
+    $total = $a + $b + $c;
+
+    // if ($total < 0) {
+    //     return 0;
+    // } else {
+    //     return $total;
+    // }
+    // 条件演算子の簡略版 return 条件 ? 値 : 値
+    return $total < 0 ? 0 : $total;
 }
 
-showAd('Header Ad'); // 実引数
-echo 'Tom is great!' . PHP_EOL;
-echo 'Bob is great!' . PHP_EOL;
-showAd();
-echo 'Steve is great!' . PHP_EOL;
-echo 'Bob is great!' . PHP_EOL;
-showAd('Footer Ad');
-
-// #23 return / #24 scope
-
-$rate = 1.1; // グローバルスコープ
-
-function sum($a, $b, $c)
-{
-    // echo $a + $b + $c .PHP_EOL;
-    // global $rate;
-    $rate = 1.08; // ローカルスコープ
-    return ($a + $b + $c) * $rate;
-    // return のあとは実行されない。
-    echo 'Here!' . PHP_EOL;
-}
-
-// sum(100, 200, 300);
-// sum(300, 400, 500);
-
-echo sum(100, 200, 300) + sum(300, 400, 500) . PHP_EOL;
+echo cal(100, 300, 500) . PHP_EOL;
+echo cal(-1000, 300, 500) . PHP_EOL;
