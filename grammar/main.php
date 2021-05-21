37b5957ce5e73ab4af356f567069b886fb494942
taaako <ta12ko.sa@gmail.com>
2021-05-21 12:33:37 +0900

    dot install -#34

--- grammar/main.php
+++ grammar/main.php
@@ -1,19 +1,23 @@
 <?php
 
-$moreScores = [
-    55,
-    72,
-    'perfect',
-    [90, 42, 88],
-];
+// ...で仮引数に何個でも入れられる -> 可変長引数
+function getStats(...$numbers)
+{
+    $total = 0;
+    foreach ($numbers as $number) {
+        $total += $number;
+    }
+    // 配列の個数 -> count
+    return [$total, $total / count($numbers)];
+}
 
-$scores = [
-    90,
-    40,
-    // 配列のなかに配列をいれると多重になってしまう-> ...をつける(php7.4以上)
-    ...$moreScores,
-    100,
-];
+// echo sum(1, 3, 5) . PHP_EOL;
+// echo sum(4, 2, 5, 1) . PHP_EOL;
+// print_r(getStats(1, 3, 5));
 
-print_r($scores);
-echo $scores[2][2] . PHP_EOL;
+// 複数の返り値を受け取る。どちらも同じ。
+// list($sum, $average) = getStats(1, 3, 5);
+[$sum, $average] = getStats(1, 3, 5);
+
+echo $sum . PHP_EOL;
+echo $average . PHP_EOL;
