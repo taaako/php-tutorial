c277fa814b8bbaea618f8084107f701d92941dfc
taaako <ta12ko.sa@gmail.com>
2021-05-21 12:09:12 +0900

    dot install #29-#31

--- grammar/main.php
+++ grammar/main.php
@@ -1,24 +1,26 @@
 <?php
 
-// 型指定するだけでは文字列の数値をintに変換してしまう。(弱い)
-// なので、強い型付けをしたければ declare文を最初につける。
-declare(strict_types=1);
+$scores = [
+    'first'  => 90,
+    'second' => 40,
+    'third'  => 100, // 配列の最後に,をつけても大丈夫
+];
 
-function showInfo(string $name, int $score): void
-{
-    echo $name . ': ' . $score . PHP_EOL;
+// keyを指定していなければ連番
+// $scores[1] = 60;
+// echo $scores[1] . PHP_EOL;
+
+// var_dump($scores);
+// print_r($scores);
+
+// echo $scores['third'] . PHP_EOL;
+
+// #31
+foreach ($scores as $score) {
+    echo $score . PHP_EOL;
 }
 
-showInfo('sato', 40);
-// Argument 2 のエラー (2番目の引数のエラー)
-// showInfo('sato', '4');
-
-// 返り値の型に？をつけると、nullか指定した型かのどちらかであるという指定ができる。
-function getAward(int $score): ?string
-{
-    return $score >= 100 ? 'Gold Medal' : null;
+// key は $key で表示可能
+foreach ($scores as $key => $score) {
+    echo $key . ' - ' . $score . PHP_EOL;
 }
-
-echo getAward(150) . PHP_EOL;
-// nullになるので、？がなければエラーになる。
-echo getAward(40) . PHP_EOL;
