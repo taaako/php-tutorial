a7f89ae3cfe6fd196d14ccc91008cfd5fa8ccd17
taaako <ta12ko.sa@gmail.com>
2021-05-21 12:17:05 +0900

    dot install #32

--- grammar/main.php
+++ grammar/main.php
@@ -1,26 +1,19 @@
 <?php
 
-$scores = [
-    'first'  => 90,
-    'second' => 40,
-    'third'  => 100, // 配列の最後に,をつけても大丈夫
+$moreScores = [
+    55,
+    72,
+    'perfect',
+    [90, 42, 88],
 ];
 
-// keyを指定していなければ連番
-// $scores[1] = 60;
-// echo $scores[1] . PHP_EOL;
+$scores = [
+    90,
+    40,
+    // 配列のなかに配列をいれると多重になってしまう-> ...をつける(php7.4以上)
+    ...$moreScores,
+    100,
+];
 
-// var_dump($scores);
-// print_r($scores);
-
-// echo $scores['third'] . PHP_EOL;
-
-// #31
-foreach ($scores as $score) {
-    echo $score . PHP_EOL;
-}
-
-// key は $key で表示可能
-foreach ($scores as $key => $score) {
-    echo $key . ' - ' . $score . PHP_EOL;
-}
+print_r($scores);
+echo $scores[2][2] . PHP_EOL;
